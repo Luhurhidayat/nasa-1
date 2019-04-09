9f0db90af3ee93b6f6894da6e65862e38bb93a7c
Luhurhidayat <45382134+Luhurhidayat@users.noreply.github.com>
2019-04-09 15:33:41 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=9FVHAn9pXqH3VC1RqZdSIJsNCybBTVejebA0jy6f";
         
         $("#loadImage").click(function(){
             $.ajax({
